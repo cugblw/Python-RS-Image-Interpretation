fb5303581354a5024dab4f24b22716ef8059868b
liwei <liwei@cennavi.com.cn>
2022-01-25 15:44:20 +0800

    set nodata

--- demo/gdal/set_nodata.py
+++ demo/gdal/set_nodata.py
@@ -1,29 +1,13 @@
-# """ 
-# from distutils import command
-# import os
-# from osgeo import gdal
-# from osgeo_utils import gdal_edit as ge
-
-# input_tif = r"C:\Users\Administrator\Desktop\Image_Src\2m\DZGYYQ.tif"
-# output_vrt = r"C:\Users\Administrator\Desktop\Image_Src\2m\DZGYYQ.vrt"
-# output_tif = r"C:\Users\Administrator\Desktop\Image_Src\2m\DZGYYQ_res.tif"
-
-# command = "gdal_translate -of GTiff -a_nodata ${na} " + input_tif + " " + output_tif
-# command1 = "gdalbuildvrt -of GTIFF -srcnodata 0 " + input_tif + output_vrt
-# command2 = "gdal_translate -of GTIFF -scale -a_nodata 0 " + output_vrt + output_tif
-# command4 = ['', '-unsetnodata', input_tif]
-
-# # os.system(command)
-# # os.system(command1)
-# # os.system(command2)
-# ge.main(command4) 
-# """
-
 import os
+import site
 from osgeo import gdal
 from osgeo_utils import gdal_edit as ge
 
-input_tif = r"C:\Users\cugbl\Desktop\DZGYYQ.tif"
+for item in site.getsitepackages():
+    if "/lib/site-packages" in item.replace("\\", "/"):
+        os.environ['PROJ_LIB'] = os.path.join(item, 'pyproj/proj_dir/share/proj')
+
+input_tif = r"C:\Users\Administrator\Desktop\Image_Src\2m\DZGYYQ.tif"
 img = gdal.Open(input_tif)
 
 for i in range(3):
@@ -32,5 +16,10 @@ for i in range(3):
     del band
 del img
 
-command = ['', '-unsetnodata', input_tif]
-ge.main(command)
+# unset nodata
+command1 = ['', '-unsetnodata', input_tif]
+ge.main(command1)
+
+# set white(255,255,255) to nodata and unset nodata
+command2 = ['', '-mo', "NODATA_VALUES=255 255 255", '-unsetnodata',input_tif]
+ge.main(command2)
