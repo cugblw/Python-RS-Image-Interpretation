5f4cf334a060e0d0ee444936661737d263ebe82d
liwei <liwei@cennavi.com.cn>
2022-03-25 13:37:59 +0800

    gdal read vrt as dataset

--- demo/gdal/gdal_multi_files_and_VRT.py
+++ demo/gdal/gdal_multi_files_and_VRT.py
@@ -1,4 +1,5 @@
 import glob
+from operator import contains
 import os
 import site
 from osgeo import gdal
@@ -8,26 +9,37 @@ for item in site.getsitepackages():
         os.environ['PROJ_LIB'] = os.path.join(item, 'pyproj/proj_dir/share/proj')
 
 in_directory = r'C:\Users\Administrator\Desktop\dem_tile'
-files_to_process = glob.glob(os.path.join(in_directory, '*.tif'))
-# file_list = []
-for data_path in files_to_process:
-
-    raster_dataset = gdal.Open(data_path, gdal.GA_ReadOnly)
-    del raster_dataset
-    #do your processing on the raster dataset here
+# files_to_process = glob.glob(os.path.join(in_directory, '*.tif'))
+files_to_process = []
+for root,dirs,files in os.walk(in_directory):
+    for file in files:
+        print(type(os.path.join(root,file)))
+        if "merge" in os.path.join(root,file):
+            del file
+        else:
+            files_to_process.append(os.path.join(root,file))
 # vrt_options = gdal.BuildVRTOptions(resampleAlg='cubic', addAlpha=True)
 vrt_options = gdal.BuildVRTOptions(resampleAlg='cubic')
 mosaicvrt = gdal.BuildVRT(r"C:\Users\Administrator\Desktop\dem_tile\merge.vrt",files_to_process,options=vrt_options)
 # dataset = gdal.Open(mosaicvrt)
 data = mosaicvrt.ReadAsArray()
+del files_to_process
+if os.path.exists(r"C:\Users\Administrator\Desktop\dem_tile\merge.tif"):
+    os.remove(r"C:\Users\Administrator\Desktop\dem_tile\merge.tif")
+
+    
 gdal.Translate(r"C:\Users\Administrator\Desktop\dem_tile\merge.tif",mosaicvrt,format='gtiff' )
-
-
 
 print(type(data))
 print(data.shape)
 del data
 
-dataset = gdal.Open(r"C:\Users\Administrator\Desktop\dem_tile\ASTGTMV003_N40E116_dem_1_1.tif")
-data = dataset.ReadAsArray()
-print(data.shape)
+tif_path = r"C:\Users\Administrator\Desktop\Image\beijing_clip_4326.tif"
+vrt_options = gdal.BuildVRTOptions(resampleAlg='cubic')
+temp_vrt = gdal.BuildVRT("temp.vrt",tif_path,options=vrt_options)
+
+dataset = temp_vrt.GetRasterBand(1).ReadAsArray()
+print(type(dataset))
+print(dataset.shape)
+
+del dataset
