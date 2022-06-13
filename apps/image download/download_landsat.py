ad748a0aff011b8ed559f268963505f4a29b9f6c
Weil Lee <cugblw204@outlook.com>
2022-06-14 00:04:16 +0800

    download

--- apps/image download/download_landsat.py
+++ apps/image download/download_landsat.py
@@ -1,7 +1,28 @@
-from landsatxplore.earthexplorer import EarthExplorer
 
-ee = EarthExplorer('Weil_Lee', 'WeilLee|2021')
+import json
+from landsatxplore.api import API
 
-ee.download('LT51960471995178MPS00', output_dir='./data')
+# Initialize a new API instance and get an access key
+api = API('Weil_Lee', 'WeilLee|2021')
 
-ee.logout()
+# Search for Landsat TM scenes
+scenes = api.search(
+    dataset='landsat_tm_c1',
+    latitude=50.85,
+    longitude=-4.35,
+    start_date='1995-01-01',
+    end_date='1995-10-01',
+    max_cloud_cover=10
+)
+
+print(f"{len(scenes)} scenes found.")
+
+# Process the result
+for scene in scenes:
+    print(scene['acquisition_date'])
+    # Write scene footprints to disk
+    fname = f"{scene['landsat_product_id']}.geojson"
+    with open(fname, "w") as f:
+        json.dump(scene['spatialCoverage'], f)
+
+api.logout()
