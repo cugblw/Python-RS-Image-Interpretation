2609b8bb3fd01e98f452c1dced2b55096b0e12c4
liwei <liwei@cennavi.com.cn>
2022-07-01 14:15:54 +0800

    索引服务实现

---
+++ demo/utils/add_watermark_to_tile.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+
+'''
+@File    :   add_watermark_to_tile.py
+@Time    :   2022/07/01 14:14:09
+@Author  :   Lee
+@Version :   1.0
+@License :   (C)Copyright Cennavi, Li Wei
+@Desc    :   None
+'''
+
